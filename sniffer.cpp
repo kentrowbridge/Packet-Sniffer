c7f834440087d0e67b33748da7ffda77ae7d4827
Kenny Trowbridge <kenny.trowbridge@gmail.com>
2015-04-19 13:59:16 -0700

    working storage and stat printing

--- sniffer.cpp
+++ sniffer.cpp
@@ -21,10 +21,13 @@ typedef struct linkNodeTag {
 //method declarations
 bool processPacket(const PDU &pdu);
 linkNode* makeNode(IPv4Address addr, int size);
-
-
+bool searchList(const IPv4Address addr, const int size);
+void insertNode(linkNode* node);
+void printList(linkNode* head);
 
 //variables
+static const int SNIFF_TIME = 60;
+
 time_t start;
 //initialize the storage list
 linkNode* head = NULL;
@@ -47,13 +50,16 @@ int main(){
 	sniffer.sniff_loop(processPacket);
 
 	std::cout<<"Finished!"<<std::endl;
+
+	printList(head);
+
 	return 0;
 }
 
 //process each packet to calculate network usage
 bool processPacket(const PDU &pdu) {
 	//time check, if past 2 minutes, stop looping
-	if(difftime(time(NULL), start) >= 120)return false;
+	if(difftime(time(NULL), start) >= SNIFF_TIME)return false;
 
 	//create a raw pdu to retrieve data for pay load size
 	const RawPDU &raw = pdu.rfind_pdu<RawPDU>();
@@ -66,7 +72,7 @@ bool processPacket(const PDU &pdu) {
 	IPv4Address local;
 
 	//defining local as any address whose first 8 bits = 10
-	AddressRange<IPv4Address> localRange = IPv4Address("10.0.0.1") / 8;
+	AddressRange<IPv4Address> localRange ("10.0.0.1", "10.255.255.255");
 	
 	//if the src is local, set local to src, else local = dst
 	local = localRange.contains(ip.src_addr()) ? ip.src_addr() 
@@ -74,7 +80,14 @@ bool processPacket(const PDU &pdu) {
 
 	//examine list for local address, if it is already there
 	//increment count and data size, else create a node for it
-	//INPROGRESS
+	if(!searchList(local, raw.payload_size()))
+	{
+		//if it is not found, add it to the tail
+		linkNode* newNode = makeNode(local, raw.payload_size());
+
+		//insert it at the tail
+		insertNode(newNode);
+	}
 
 	//print to stdout
 	std::cout << ip.src_addr() << "  ->  " 
@@ -85,8 +98,21 @@ bool processPacket(const PDU &pdu) {
 	return true;
 }
 
+void printList(linkNode* head) {
+	linkNode* iterator = head;
+	while(iterator != NULL)
+	{
+		std::cout << "====" << iterator->ip <<"===="<<std::endl;
+		std::cout << "	number of packets: " << iterator->count <<std::endl;
+		std::cout << "	total data received/sent: " << iterator->totalData << std::endl;
+		std::cout << "==============" << std::endl;
+		iterator = iterator->next;
+	}
+
+}
+
 //search the storage list for this ip node
-bool searchList(const IPv4Address addr) {
+bool searchList(const IPv4Address addr, const int size) {
 
 	linkNode* iterator = head;
 	while(iterator != NULL)
@@ -95,8 +121,10 @@ bool searchList(const IPv4Address addr) {
 		{	
 			//increment count on this node
 			iterator->count++;
+			iterator->totalData += size;
 			return true;
 		}
+		iterator = iterator->next;
 	}
 	//not found in list
 	return false;
@@ -104,11 +132,13 @@ bool searchList(const IPv4Address addr) {
 
 //insert a node into the given list
 void insertNode(linkNode* node) {
+	if(node == NULL) return;
 	//copy list head
 	linkNode* iterator = head;
 	if(iterator == NULL)
 	{//if the list is empty, insert node at beginning
 		head = node;
+		return;
 	}
 
 	while(iterator->next != NULL)
@@ -123,13 +153,13 @@ void insertNode(linkNode* node) {
 linkNode* makeNode(const IPv4Address addr, const int size) {
 	//error check
 	if(size < 1) return NULL;
-
+	
 	//create node pointer
 	linkNode* newNode = (linkNode*)malloc(sizeof(linkNode));
 
 	//error check
 	if(newNode == NULL) return NULL;
-	
+
 	//initialize node values
 	newNode->totalData = size;
 	newNode->count = 1;
