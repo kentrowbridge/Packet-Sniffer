5f6158ed5ab05a66b5f17f92838df98094a42807
Kenny Trowbridge <kenny.trowbridge@gmail.com>
2015-04-20 10:50:53 -0700

    added documentation

--- sniffer.cpp
+++ sniffer.cpp
@@ -19,7 +19,7 @@ typedef struct LinkNodeTag {
 //method declarations
 bool processPacket(const PDU &pdu);
 LinkNode* makeNode(IPv4Address addr, int size);
-bool searchList(const IPv4Address addr, const int size);
+bool searchList(const IPv4Address addr, const int size, LinkNode* head);
 void insertNode(LinkNode* node);
 void printList(LinkNode* head);
 LinkNode* merge(LinkNode* left, LinkNode* right);
@@ -46,6 +46,7 @@ int main(){
 	Sniffer sniffer("wlan0", config);
 
 	std::cout << "Sniffing Packets..." << std::endl;
+	std::cout << "src -> dst : size" <<std::endl;
 
 	//each packet received will be processed
 	sniffer.sniff_loop(processPacket);
@@ -59,7 +60,14 @@ int main(){
 	return 0;
 }
 
-//process each packet to calculate network usage
+/* processPacket()
+ *
+ * This method is called on every packet that the sniffloop picks up.
+ * When the method returns true, the loop continues and when the method
+ * returns false, the loop is ended.  This method only returns false when
+ * a specified amount of time has passed.
+ *
+ */
 bool processPacket(const PDU &pdu) {
 	//time check, if past 2 minutes, stop looping
 	if(difftime(time(NULL), start) >= SNIFF_TIME)return false;
@@ -76,11 +84,11 @@ bool processPacket(const PDU &pdu) {
 	
 	//if the src is local, set local to src, else local = dst
 	local = localRange.contains(ip.src_addr()) ? ip.src_addr() 
-											: ip.dst_addr();
+											   : ip.dst_addr();
 
 	//examine list for local address, if it is already there
 	//increment count and data size, else create a node for it
-	if(!searchList(local, raw.payload_size()))
+	if(!searchList(local, pdu.size(), head))
 	{
 		//if it is not found, add it to the tail
 		LinkNode* newNode = makeNode(local, pdu.size());
@@ -89,66 +97,95 @@ bool processPacket(const PDU &pdu) {
 		insertNode(newNode);
 	}
 
-	//print to stdout
+	//print packet src -> dst : size to stdout
 	std::cout << ip.src_addr() << "  ->  " 
-		<< ip.dst_addr() << " : " << raw.payload_size()
+		<< ip.dst_addr() << " : " << pdu.size()
 		<<std::endl;
 
 	//continue
 	return true;
 }
 
+/* printList()
+ *
+ * Method takes a linked list and prints out the first 5
+ * entries.
+ *
+ */
 void printList(LinkNode* head) {
-	LinkNode* iterator = head;
-	while(iterator != NULL)
+	int counter = 0;
+	//print out the first five entries in the list
+	while(head != NULL && counter <= 5)
 	{
-		std::cout << "====" << iterator->ip <<"===="<<std::endl;
-		std::cout << "	number of packets: " << iterator->count <<std::endl;
-		std::cout << "	total data received/sent: " << iterator->totalData << std::endl;
-		std::cout << "==============" << std::endl;
-		iterator = iterator->next;
+		std::cout << "====" << head->ip <<"===="<<std::endl;
+		std::cout << "	number of packets: " << head->count <<std::endl;
+		std::cout << "	total data received/sent: " << head->totalData << std::endl;
+		//move to next link
+		head = head->next;
+		counter++;
 	}
 }
 
-//search the storage list for this ip node
-bool searchList(const IPv4Address addr, const int size) {
+/* searchList()
+ *
+ * addr - IP address to search for
+ * size - how much to increment the size of a matching IP link
+ * head - pointer to the linked list
+ * 
+ * This method searches the given linked list for the specified IP address. 
+ * If it is found, it increments the number of packets found by 1 and the 
+ * total data by the size parameter, and then returns true. Returns false 
+ * if IP address is not in the list.
+ *
+ */
+bool searchList(const IPv4Address addr, const int size, LinkNode* head) {
+	//param check
+	if(size < 0 || head == NULL) return false;
 
-	LinkNode* iterator = head;
-	while(iterator != NULL)
+	while(head != NULL)
 	{//if this ip address is in the list return true
-		if(iterator->ip == addr) 
+		if(head->ip == addr) 
 		{	
 			//increment count on this node
-			iterator->count++;
-			iterator->totalData += size;
+			head->count++;
+			head->totalData += size;
 			return true;
 		}
-		iterator = iterator->next;
+		head = head->next;
 	}
 	//not found in list
 	return false;
 }
 
-//insert a node into the given list
+/* insertNode()
+ *
+ * Method takes a linknode and inserts it at the end of the list
+ *
+ */
 void insertNode(LinkNode* node) {
 	if(node == NULL) return;
 	//copy list head
-	LinkNode* iterator = head;
-	if(iterator == NULL)
+	if(head == NULL)
 	{//if the list is empty, insert node at beginning
 		head = node;
 		return;
 	}
 
-	while(iterator->next != NULL)
+	while(head->next != NULL)
 	{//find the end of the list
-		iterator = iterator->next;
+		head = head->next;
 	}
 	//instert the node
-	iterator->next = node;
+	head->next = node;
 }
 
-// a node out of the given address and data size
+/* makeNode()
+ *
+ * Method takes an IP address and the size of a packet and creates
+ * a new node that keeps statistics for the specified IP address
+ *
+ * returns a pointer to the newly created node
+ */
 LinkNode* makeNode(const IPv4Address addr, const int size) {
 	//error check
 	if(size < 1) return NULL;
@@ -168,7 +205,14 @@ LinkNode* makeNode(const IPv4Address addr, const int size) {
 	return newNode;
 }
 
-//merge two lists together and return the result
+/* merge()
+ *
+ * Method takes two pointers to linked lists and merges them based on 
+ * the values of the leading links. It compares each node in the list
+ * and inserts the greater of the two into the new list.  
+ *
+ * returns a pointer to the newly merged list
+ */
 LinkNode* merge(LinkNode* left, LinkNode* right) {
 	if(left == NULL) return right;
 	if(right == NULL) return left;
@@ -189,6 +233,12 @@ LinkNode* merge(LinkNode* left, LinkNode* right) {
 	return head;
 }
 
+
+/* mergeSort()
+ *
+ * Method takes a linked list and sorts it using merge sort
+ * returns a pointer to the newly sorted list
+ */
 LinkNode* mergeSort(LinkNode* head) {
 	//error
 	if(head == NULL){return head;}
@@ -218,6 +268,11 @@ LinkNode* mergeSort(LinkNode* head) {
 	return merge(mergeSort(left), mergeSort(right));
 }
 
+/* count()
+ *
+ * Method takes a linked list and returns its length
+ *
+ */
 int count(LinkNode* head) {
 	int counter = 0;
 	while(head != NULL)
